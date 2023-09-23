4c8d00a72167460ba0bba32a6ac6b9084cec6c5b
Oli-Dom <oadl12345678@gmail.com>
2023-09-23 17:45:04 -0400

    Finished MVP, no random yet

---
+++ README.md
@@ -0,0 +1,3 @@
+Project 2 of codePath intermediate web dev
+
+Flash cards
---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import './App.css';
+function App() {
+  const data = [
+    {
+      description:
+        "loved by celebrities, drools a lot, and can't swim due to their flat nose",
+      breed: 'French Bulldog',
+    },
+    {
+      description:
+        'Are often in movies, loves tennis balls, and is easy to train',
+      breed: 'Golden Retriever',
+    },
+    {
+      description:
+        'Originated in the mountains of Europe, common to have two different color eyes, often associated with cowboy culture',
+      breed: 'Australian Shepherd',
+    },
+  ];
+
+  const [count, setCount] = useState(data.length);
+  const [text, setText] = useState(data[0].description);
+  const [iTracker, setTracker] = useState(0);
+
+  const addCard = () => {
+    const userBreed = window.prompt('Enter the breed:');
+    const userFacts = window.prompt('Enter a fact:');
+    const newData = {
+      description: userFacts,
+      breed: userBreed,
+    };
+    data.push(newData);
+    setCount(count + 1);
+  };
+
+  const showAnswer = () => {
+    setText(data[iTracker].breed);
+  };
+
+  const nextCard = () => {
+    setTracker(iTracker + 1);
+    setText(data[iTracker + 1].description);
+  };
+
+  return (
+    <div>
+      <div className="header">
+        <h1>Test your dog knowledge!</h1>
+        <h2>Lets see how much you know about dog breeds</h2>
+        <h3>Number of cards: {count}</h3>
+      </div>
+      <div className="container">
+        <div className="card-container" onClick={showAnswer}>
+          {text}
+        </div>
+        <div className="userActions">
+          <button onClick={nextCard}>next</button>
+          <button onClick={addCard}>Add a Breed</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
